7e61e29b46edaae2075d316482807d5bc1a697f6
Manoel Neto <manoel180@hotmail.com>
2014-06-25 06:37:02 -0400

    Import initial

---
+++ src/carnefacil/carnefacil-services/src/main/java/br/com/carnefacil/services/TaskDao.java
@@ -0,0 +1,30 @@
+package br.com.carnefacil.services;
+
+import br.com.carnefacil.web.model.Task;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class TaskDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Transactional
+	public void save(Task task) {
+		entityManager.persist(task);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Task> list() {
+		return entityManager.createQuery("select t from Task t")
+				.getResultList();
+	}
+
+}
---
+++ src/carnefacil/carnefacil-web/src/main/java/br/com/carnefacil/web/utls/converters/EmptyToNullConverter.java
@@ -0,0 +1,25 @@
+package br.com.carnefacil.web.utls.converters;
+
+import javax.faces.component.EditableValueHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass = String.class)
+public class EmptyToNullConverter implements Converter {
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+        if (value == null || value.trim().length() == 0) {
+            if (component instanceof EditableValueHolder) {
+                ((EditableValueHolder) component).setSubmittedValue(null);
+            }
+            return null;
+        }
+        return value;
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
+        return (value == null) ? null : value.toString();
+    }
+}
