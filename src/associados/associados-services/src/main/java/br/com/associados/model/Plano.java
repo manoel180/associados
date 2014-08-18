057cd178b3c5280e122288c5371b4f3dba3efc97
Manoel Neto <manoel180@hotmail.com>
2014-08-18 09:54:59 -0400

    add servicos

--- src/associados/associados-services/src/main/java/br/com/associados/model/Plano.java
+++ src/associados/associados-services/src/main/java/br/com/associados/model/Plano.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity(name="Planos")
+@XmlRootElement
 public class Plano {
 
 	@Id
--- src/associados/associados-services/src/main/java/br/com/associados/services/PlanoService.java
+++ src/associados/associados-services/src/main/java/br/com/associados/services/PlanoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,6 +17,7 @@ import br.com.associados.model.Plano;
 * Root resource (exposed at "myresource" path)
 */
 @Path("/planos")
+@Produces(MediaType.APPLICATION_JSON)
 public class PlanoService {
 	
 	@Autowired
@@ -28,8 +30,30 @@ public class PlanoService {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Plano> getIt() {
+    public List<Plano> getPlano() {
         return planoDao.list();
+    }
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Path("{id}")
+    public Plano getPlano(@PathParam("id") Integer id) {
+    	return planoDao.list().get(id);
+    }
+    
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Path("{id}/{status}")
+    public Plano getPlano(@PathParam("id") Integer id, @PathParam("status") Boolean status) {
+    	return planoDao.list().get(id);
     }
 }
